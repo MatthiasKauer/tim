5b57001adb467d7a0541cfe93505de6c37dfe46c
Matthias Kauer <mk.software@zuez.org>
2015-06-22 13:12:34 +0800

    moved bin/tim to subdir and brought over preamble for devel

---
+++ bin/ccrun.py
@@ -0,0 +1,15 @@
+"""
+starter script to be used during development (where paths would otherwise be messed up)
+
+    :copyright: 2015 by Matthias Kauer
+    :license: BSD
+"""
+import sys
+
+try:
+    import _preamble
+except ImportError:
+    sys.exit(-1)
+
+from croncoat.scripts.ccscript import main
+main()
