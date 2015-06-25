2e4f9255392a3cfd2847ad19fa5e7528f8eb4247
Matthias Kauer <mk.software@zuez.org>
2015-06-25 21:31:18 +0800

    deleted code that I don't think I'll ever use

--- tim/timscript.py
+++ tim/timscript.py
@@ -151,59 +151,6 @@ def action_fin(time, back_from_interrupt=True):
         else:
             print('Congrats, you\'re out of interrupts!')
 
-
-#  def action_interrupt(name, time):
-#      ensure_working()
-#  
-#      action_fin(time, back_from_interrupt=False)
-#  
-#      data = store.load()
-#      if 'interrupt_stack' not in data:
-#          data['interrupt_stack'] = []
-#      interrupt_stack = data['interrupt_stack']
-#      work = data['work']
-#  
-#      interrupted = data['work'][-1]
-#      interrupt_stack.append(interrupted)
-#      store.dump(data)
-#  
-#      action_on('interrupt: ' + green(name), time)
-#      print('You are now %d deep in interrupts.' % len(interrupt_stack))
-
-
-#  def action_note(content):
-#      ensure_working()
-#  
-#      data = store.load()
-#      current = data['work'][-1]
-#  
-#      if 'notes' not in current:
-#          current['notes'] = [content]
-#      else:
-#          current['notes'].append(content)
-#  
-#      store.dump(data)
-#  
-#      print('Yep, noted to ' + yellow(current['name']) + '.')
-#  
-
-#  def action_tag(tags):
-#      ensure_working()
-#  
-#      data = store.load()
-#      current = data['work'][-1]
-#  
-#      current['tags'] = set(current.get('tags') or [])
-#      current['tags'].update(tags)
-#      current['tags'] = list(current['tags'])
-#  
-#      store.dump(data)
-#  
-#      tag_count = str(len(tags))
-#      print('Okay, tagged current work with ' + tag_count + ' tag' +
-#              ('s' if tag_count > 1 else '') + '.')
-#  
-
 def action_status():
     try:
         ensure_working()
@@ -240,17 +187,8 @@ def action_hledger(param):
 
     hlfile.close()
 
-    #  split_param = shlex.split(param)
-    #  print("split_param", split_param)
-
-    #  cmd_str = "hledger" + param
-    #  print("cmd_str", cmd_str)
-    #  subprocess.call(cmd_str, shell=True)
-
     cmd_list = ['hledger'] + ['-f'] + [hlfname] + param
     print("tim executes: " + " ".join(cmd_list))
-    #  subprocess.call(['ls', '-l'])
-    #  subprocess.call(cmd_list, shell=True)
     subprocess.call(cmd_list) 
 
 def action_ini():
@@ -261,50 +199,6 @@ def action_ini():
     print("#I suggest you call tim ini > ~/.tim.ini to start using this optional config file")
 
     print(out_str.getvalue())
-
-
-
-# def action_log(period):
-#     data = store.load()
-#     work = data['work'] + data['interrupt_stack']
-#     log = defaultdict(lambda: {'delta': timedelta()})
-#     current = None
-# 
-#     for item in work:
-#         start_time = parse_isotime(item['start'])
-#         if 'end' in item:
-#             log[item['name']]['delta'] += parse_isotime(item['end']) - start_time
-#         else:
-#             log[item['name']]['delta'] += datetime.utcnow() - start_time
-#             current = item['name']
-# 
-#     name_col_len = 0
-# 
-#     for name, item in log.items():
-#         name_col_len = max(name_col_len, len(name))
-# 
-#         secs = item['delta'].seconds
-#         tmsg = []
-# 
-#         if secs > 3600:
-#             hours = int(secs / 3600)
-#             secs -= hours * 3600
-#             tmsg.append(str(hours) + ' hour' + ('s' if hours > 1 else ''))
-# 
-#         if secs > 60:
-#             mins = int(secs / 60)
-#             secs -= mins * 60
-#             tmsg.append(str(mins) + ' minute' + ('s' if mins > 1 else ''))
-# 
-#         if secs:
-#             tmsg.append(str(secs) + ' second' + ('s' if secs > 1 else ''))
-# 
-#         log[name]['tmsg'] = ', '.join(tmsg)[::-1].replace(',', ' &'[::-1], 1)[::-1]
-# 
-#     for name, item in sorted(log.items(), key=(lambda x: x[1]), reverse=True):
-#         print(name.ljust(name_col_len), ' ∙∙ ', item['tmsg'],
-#                 end=' ← working\n' if current == name else '\n')
-# 
 
 #  def action_edit():
 #      if 'EDITOR' not in os.environ:
@@ -490,31 +384,6 @@ def parse_args(argv=sys.argv):
     elif head in ['ini']:
         fn = action_ini
         args = {}
-
-    #  elif head in ['t', 'tag']:
-    #      if not tail:
-    #          helpful_exit('Please provide at least one tag to add.')
-#  
-    #      fn = action_tag
-    #      args = {'tags': tail}
-
-    #  elif head in ['n', 'note']:
-    #      if not tail:
-    #          helpful_exit('Please provide some text to be noted.')
-#  
-    #      fn = action_note
-    #      args = {'content': ' '.join(tail)}
-
-    #  elif head in ['i', 'interrupt']:
-    #      if not tail:
-    #          helpful_exit('Need the name of whatever you are working on.')
-#  
-    #      fn = action_interrupt
-    #      args = {
-    #          'name': tail[0],
-    #          'time': to_datetime(' '.join(tail[1:])),
-    #      }
-#  
     else:
         helpful_exit("I don't understand '" + head + "'")
 
