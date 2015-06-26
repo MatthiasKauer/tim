bd392dd88d2b6702319f888d4689e9e7b37151aa
Matthias Kauer <mk.software@zuez.org>
2015-06-26 13:13:35 +0800

    added pytz tzlocal in pdt_test -> now time saving works as I'd like

--- pdt_test.py
+++ pdt_test.py
@@ -3,14 +3,21 @@ import parsedatetime as pdt
 import sys
 import pytz
 
+# http://stackoverflow.com/questions/13218506/how-to-get-system-timezone-setting-and-pass-it-to-pytz-timezone
+from tzlocal import get_localzone # $ pip install tzlocal
+local_tz = get_localzone()
+
 def parse_args(argv=sys.argv):
     cal = pdt.Calendar()
     timestr = ' '.join(argv)
-    ret = cal.parseDT(timestr, sourceTime=datetime.utcnow(), tzinfo=pytz.utc)[0]
-
-    # ret = make_local(ret)
-
+    # ret = cal.parseDT(timestr, sourceTime=datetime.utcnow(), tzinfo=pytz.utc)[0]
+    ret = cal.parseDT(timestr, tzinfo=local_tz)[0]
+    ret_utc = ret.astimezone(pytz.utc)
     print(ret)
+    print(ret_utc)
+
+    ret_loc = ret_utc.astimezone(local_tz)
+    print(ret_loc)
 
 if __name__ == '__main__':
     parse_args()
