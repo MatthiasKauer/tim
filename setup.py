898ffd7157d68da17b33ec29b2b7ba9f6e200842
Matthias Kauer <mk.software@zuez.org>
2015-09-13 11:07:37 +0800

    adjusted setup.py for PyPI

--- setup.py
+++ setup.py
@@ -3,19 +3,39 @@ import os
 from setuptools import setup, find_packages
 from tim import __version__
 
+#PyPI guide: https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
-setup(name='tim',
-        version = __version__,
-        install_requires=[
-            'argparse>=1.1',
-            'parsedatetime>=1.2',
-            'colorama>=0.3.0',
-            'pyyaml>=3.0',
-            'tzlocal>=1.2',
-            'pytz>=2015.2',
-            ],
-        #  packages = ['cronwrap/cw', 'cronwrap/scripts'],
-        packages = find_packages(),
-        entry_points={'console_scripts': ['tim=tim.timscript:main']}
-        )
+setup(
+    name='tim',
+    version = __version__,
+    install_requires=[
+        'argparse>=1.1',
+        'parsedatetime>=1.2',
+        'colorama>=0.3.0',
+        'pyyaml>=3.0',
+        'tzlocal>=1.2',
+        'pytz>=2015.2',
+        ],
+    packages = find_packages(),
+    entry_points={'console_scripts': ['tim=tim.timscript:main']},
+    description="command line time logger with hledger backend for number crunching",
+    long_description=(read('README.md') + "\n\n" + 
+                    read("AUTHORS.md")),
+    license="MIT",
+    author="Matthias Kauer",
+    author_email="mk.software@zuez.org",
+    platforms=["Any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+)
 
